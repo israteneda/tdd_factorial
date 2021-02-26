f7da46b055673fcb3e9d0c5935601474cc9f4d05
Israel Teneda <israteneda@gmail.com>
2021-02-26 11:46:55 -0500

    feat: add more tests

--- factorial.py
+++ factorial.py
@@ -1,11 +1,27 @@
-class InvalidArgument(Exception):
+class InvalidArgumentException(Exception):
     pass
 
 
-def factorial(num):
+def factorial_v1(num):
     if not isinstance(num, int):
-        raise InvalidArgument("An incorrect argument was passed")
+        raise InvalidArgumentException("A string argument was passed, integer is required")
+    if num < 0:
+        raise InvalidArgumentException("A negative number was passed, integer is required")
+    if num == 0:
+        return 1
     if num == 1:
         return 1
     else:
+        return num * factorial_v1(num - 1)
+
+def factorial(num):
+    if not isinstance(num, int):
+        raise InvalidArgumentException("A string argument was passed, integer is required")
+    if num < 0:
+        raise InvalidArgumentException("A negative number was passed, integer is required")
+    if num == 0:
+        return 1
+    else:
         return num * factorial(num - 1)
+
+
--- test_factorial.py
+++ test_factorial.py
@@ -1,5 +1,6 @@
 from factorial import factorial
-from factorial import InvalidArgument
+from factorial import InvalidArgumentException
+from random import randint
 import pytest
 
 # Happy paths
@@ -8,16 +9,32 @@ import pytest
 #    assert factorial(2) == 2
 
 def test_factorial__return_the_number_one__when_the_number_is_one_v2():
+
     assert factorial(1) == 1
 
 def test_factorail__return_the_number_six__when_the_number_is_three():
+
     assert factorial(3) == 6
 
+def test_factorail__return_a_positive_number__when_the_number_is_positive():
 
-# Bad paths
+    number = randint(1, 10)
+
+    assert factorial(number) > 0 
+
+def test_factorail__return_one__when_the_number_is_zero():
+    assert factorial(0) == 1 
+
+# Sad paths
 
 def test_factorial__raise_an_exception__when_the_number_is_a_string():
-    with pytest.raises(InvalidArgument) as e:
+    with pytest.raises(InvalidArgumentException) as e:
         factorial("cinco")
 
-    assert str(e.value) == "An incorrect argument was passed"
+    assert str(e.value) == "A string argument was passed, integer is required"
+
+def test_factorial__raise_an_exception__when_the_number_is_negative():
+    with pytest.raises(InvalidArgumentException) as e:
+        factorial(-1)
+
+    assert str(e.value) == "A negative number was passed, integer is required"
