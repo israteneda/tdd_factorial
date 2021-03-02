256b98840a59d31757e7406e69391151250199f2
Israel Teneda <israteneda@gmail.com>
2021-03-02 11:31:24 -0500

    feat: GitHub CI

--- test_factorial.py
+++ test_factorial.py
@@ -6,7 +6,7 @@ import pytest
 # Happy paths
 
 def test_factorial__return_the_factorial_of_the_number__when_the_number_is_positive():
-    
+
     assert factorial(1) == 1
     assert factorial(2) == 2
     assert factorial(3) == 6
@@ -22,11 +22,17 @@ def test_factorail__return_one__when_the_number_is_zero():
 
     assert factorial(0) == 1 
 
-# Sad paths
+# Edge cases 
 
 def test_factorial__raise_an_exception__when_the_number_is_a_string():
     with pytest.raises(InvalidArgumentException) as e:
         factorial("cinco")
+
+    assert str(e.value) == NON_NUMBER_INVALID_ARGUMENT
+
+def test_factorial__raise_an_exception__when_the_number_is_float():
+    with pytest.raises(InvalidArgumentException) as e:
+        factorial(5.5)
 
     assert str(e.value) == NON_NUMBER_INVALID_ARGUMENT
 
